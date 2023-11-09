aa6c766ac1063e5974a44ab17f7033c84816fb50
hosthans <hanweb6@gmail.com>
2023-11-09 01:03:38 +0100

    refactor update

--- training/train_gan.py
+++
@@ -1,9 +0,0 @@
-from utils.trainer import Trainer
-
-def main():
-    trainer = Trainer(dataset="mnist", mode="gan")
-    # trainer.correct_module()
-    trainer.train()
-
-if __name__ == '__main__':
-    main()
